a8666abde6951f8bc29e8d40aff2f05650bb93ef
Kevin Tandon <kevintandon123@gmail.com>
2024-07-29 12:44:41 +0530

    initialized gitrepo with cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project involves creating an AI-powered virtual assistant that provides detailed walkthroughs of new automotive models. The assistant will help potential customers understand the features, benefits, and customizations available in different models. The system will use natural language processing (NLP) to interact with users and provide personalized recommendations.',
+    author='kev0-4',
+    license='',
+)
